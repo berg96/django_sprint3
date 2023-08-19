8cabed7b04a49554adf173aeef531ced72de6463
Artem Kulikov <chigar2010@yandex.ru>
2023-08-19 18:43:47 +0200

    fix

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -6,24 +6,23 @@ from blog.models import Post, Category
 
 
 def index(request):
-    post_list = Post.published().select_related(
-        'location',
-        'category',
-        'author'
-    )[0:5]
-    return render(request, 'blog/index.html', {'post_list': post_list})
-
-
-def post_detail(request, post_id):
-    post = get_object_or_404(
-        Post.published().select_related(
+    return render(request, 'blog/index.html', {
+        'post_list': Post.published().select_related(
             'location',
             'category',
             'author'
-        ),
-        pk=post_id
-    )
-    return render(request, 'blog/detail.html', {'post': post})
+        )[0:5]
+    })
+
+
+def post_detail(request, post_id):
+    return render(request, 'blog/detail.html', {
+        'post': get_object_or_404(Post.published().select_related(
+            'location',
+            'category',
+            'author'
+        ), pk=post_id)
+    })
 
 
 def category_posts(request, category_slug):
@@ -32,16 +31,11 @@ def category_posts(request, category_slug):
         slug=category_slug,
         is_published=True
     )
-    post_list = category.posts.select_related(
-        'location',
-        'category',
-        'author'
-    ).filter(
-        is_published=True,
-        pub_date__lte=datetime.now()
-    )
-    return render(
-        request,
-        'blog/category.html',
-        {'category': category, 'post_list': post_list, }
-    )
+    return render(request, 'blog/category.html', {
+        'category': category,
+        'post_list': category.posts.select_related(
+            'location',
+            'category',
+            'author'
+        ).filter(is_published=True, pub_date__lte=datetime.now()),
+    })
