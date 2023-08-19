cba6c09e2744d8e3f5c18c6619ae7493d5de90b9
Artem Kulikov <chigar2010@yandex.ru>
2023-08-19 06:19:29 +0200

    make index()

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,24 +1,33 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post, Category
+from datetime import datetime
 
 
 def index(request):
     template = 'blog/index.html'
-    context = {'posts': posts}
+    post_list = Post.objects.filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=datetime.now()
+    ).order_by('-pub_date')[0:5]
+    context = {'post_list': post_list}
     return render(request, template, context)
 
 
-def post_detail(request, post_id):
+def post_detail(request, pk):
     template = 'blog/detail.html'
-    context = None
-    if post_id in post_data:
-        context = {'post': post_data[post_id]}
-    if context is None:
-        raise Http404(f'Invalid value of the post_id = {post_id}')
+    post = Post.objects.get(pk=pk)
+    context = {'post': post}
     return render(request, template, context)
 
 
 def category_posts(request, category_slug):
     template = 'blog/category.html'
-    context = {'category': category_slug}
+    #post_list = get_object_or_404(Post.objects.select_related('category').filter(
+    #    is_published=True,
+    #    category__slug=category_slug,
+    #    pub_date__lte=datetime.now()
+    #).order_by('-pub_date'), category__is_published=True)
+    #context = {'post_list': post_list}
     return render(request, template, context)
